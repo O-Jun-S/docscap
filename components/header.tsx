99ba135f24bdf1dfacb0039c099aaf44d419a920
O-Jun-S <jun.shima256@gmail.com>
2022-07-03 12:34:31 +0900

    Add some margin to title.

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ const Header: NextPage = () => {
         <>
             <header className="flex flex-col items-center gap-3">
                   <h1 className="text-5xl text-teal-700">DocsCap</h1>
-                  <h1 className="text-3xl">斜めに撮影した書類を簡単補正</h1>
+                  <h1 className="text-3xl m-6">斜めに撮影した書類を簡単補正</h1>
             </header>
         </>
     );
