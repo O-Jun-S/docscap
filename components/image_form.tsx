c8c1a57025ddca981740ac65b6e2bbafde417720
O-Jun-S <jun.shima256@gmail.com>
2022-07-03 11:50:41 +0900

    Can get points' coordinates.

--- components/image_form.tsx
+++ components/image_form.tsx
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { RefObject } from "react";
 import type { NextPage } from "next";
 import Point from "./point";
+
+type Rect = {
+    left: number;
+    top: number;
+    right: number;
+    bottom: number;
+};
+
+type setRect = (rect: Rect) => void;
 
 const ImageForm: NextPage = () => {
     const [preview, setPreview] = useState("");
     const [is_uploaded, setIsUploaded] = useState(false);
 
+    const [point1Rect, setPoint1Rect] = useState({ left: 0, top: 0, right: 0, bottom: 0 });
+    const point1Ref = useRef<HTMLDivElement>(null!);
+    const [point2Rect, setPoint2Rect] = useState({ left: 0, top: 0, right: 0, bottom: 0 });
+    const point2Ref = useRef<HTMLDivElement>(null!);
+    const [point3Rect, setPoint3Rect] = useState({ left: 0, top: 0, right: 0, bottom: 0 });
+    const point3Ref = useRef<HTMLDivElement>(null!);
+    const [point4Rect, setPoint4Rect] = useState({ left: 0, top: 0, right: 0, bottom: 0 });
+    const point4Ref = useRef<HTMLDivElement>(null!);
+
     const handleChangeFile = (e: any) => {
         const { files } = e.target;
         setPreview(window.URL.createObjectURL(files[0]));
         setIsUploaded(true);
+    };
+
+    const onDrag = (pointRef: RefObject<HTMLDivElement>, setPointRect: setRect) => () => {
+        const point = pointRef.current;
+        if(!point) return;
+        const { left, top, right, bottom } = point.getBoundingClientRect();
+        setPointRect({ left, top, right, bottom });
+        console.log("left: " + left + ", top: " + top + ", right: " + right + ", bottom: " + bottom);
     };
 
     return (
@@ -22,10 +49,10 @@ const ImageForm: NextPage = () => {
         />
         {is_uploaded && (
             <>
-              <Point pos_x={10} pos_y={10}/>
-              <Point pos_x={50} pos_y={10}/>
-              <Point pos_x={10} pos_y={50}/>
-              <Point pos_x={50} pos_y={50}/>
+              <Point pos_x={10} pos_y={10} _ref={point1Ref} on_drag={onDrag(point1Ref, setPoint1Rect)}/>
+              <Point pos_x={50} pos_y={10} _ref={point2Ref} on_drag={onDrag(point2Ref, setPoint2Rect)}/>
+              <Point pos_x={10} pos_y={50} _ref={point3Ref} on_drag={onDrag(point3Ref, setPoint3Rect)}/>
+              <Point pos_x={50} pos_y={50} _ref={point4Ref} on_drag={onDrag(point4Ref, setPoint4Rect)}/>
             </>
         )}
       </div>
--- components/point.tsx
+++ components/point.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from "next";
 import Draggable from "react-draggable";
+import { RefObject } from "react";
 
 type Props = {
-  pos_x?: number;
-  pos_y?: number;
+    pos_x?: number;
+    pos_y?: number;
+    _ref?: RefObject<HTMLDivElement>;
+    on_drag: any;
 };
 
-const Point: NextPage<Props> = ({ pos_x, pos_y }) => {
+const Point: NextPage<Props> = ({ pos_x, pos_y, _ref, on_drag }) => {
     return (
-      <Draggable defaultPosition={{x: pos_x ? pos_x : 0, y: pos_y ? pos_y : 0}}>
-        <div className="absolute w-0.5 p-2 rounded-full bg-amber-200">
+      <Draggable nodeRef={_ref} onDrag={on_drag} defaultPosition={{x: pos_x ? pos_x : 0, y: pos_y ? pos_y : 0}}>
+        <div ref={_ref} className="absolute w-0.5 p-2 rounded-full bg-amber-200">
         </div>
       </Draggable>
     );
